c2191c553ad83ed9abc16cc2c761b111c1d833de
Ogunniyi Samuel <samuelkayode622@gmail.com>
2023-02-04 12:57:33 +0100

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,30 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AboutComponent } from './about/about.component';
+import { EducationComponent } from './education/education.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { SkillsComponent } from './skills/skills.component';
+import { LangComponent } from './lang/lang.component';
+import { InterestsComponent } from './interests/interests.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    AboutComponent,
+    EducationComponent,
+    ExperienceComponent,
+    SkillsComponent,
+    LangComponent,
+    InterestsComponent,
+    ContactComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/lang/lang.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lang',
+  templateUrl: './lang.component.html',
+  styleUrls: ['./lang.component.css']
+})
+export class LangComponent {
+
+}
